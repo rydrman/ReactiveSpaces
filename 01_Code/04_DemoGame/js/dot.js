7b2c29f7245446f676724a17dcdb37cfc7828e92
Ryan Bottriell <ryanbottriell@live.ca>
2014-11-21 18:40:59 -0500

    joint velocities

--- 01_Code/04_DemoGame/js/dot.js
+++ 01_Code/04_DemoGame/js/dot.js
@@ -45,16 +45,20 @@ Dot.prototype.collideWithBorders = function()
 {
     
     if(this.position.x-this.radius < 0){
-        this.speed.x = Math.abs(this.speed.x);
+        this.speed.x += Math.abs(this.speed.x);
+        if(this.speed.x == 0) this.acceleration.x += this.maxSpeed * 0.05;
     }
     else if(this.position.x+this.radius > canvas.width){
         this.speed.x = -Math.abs(this.speed.x);
+        if(this.speed.x == 0) this.acceleration.x 0-= this.maxSpeed * 0.05;
     }
     if(this.position.y-this.radius < 0){
         this.speed.y = Math.abs(this.speed.y);
+        if(this.speed.y == 0) this.acceleration.y += this.maxSpeed * 0.05;
     }
     else if(this.position.y+this.radius > canvas.height){
         this.speed.y = -Math.abs(this.speed.y);
+        if(this.speed.y == 0) this.acceleration.y -= this.maxSpeed * 0.05;
     }
 }
 
