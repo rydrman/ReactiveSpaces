ddd61115a6e6ab948cdc4e6d09399e67f3a77f1c
Ryan Bottriell <ryanbottriell@live.ca>
2014-11-21 16:48:45 -0500

    remote player enter event wasn't firing 6

--- 01_Code/04_DemoGame/js/game.js
+++ 01_Code/04_DemoGame/js/game.js
@@ -307,7 +307,7 @@ Game.prototype.render = function()
     //draw remote skeletons
     for(var i in RS.remotePlayers)
     {
-        RS.drawSkeleton(ctx, RS.remotePlayers[i], "rgba(255, 255, 255, 0.2)", true);
+        RS.DrawSkeleton(ctx, RS.remotePlayers[i], "rgba(255, 255, 255, 0.2)", true);
     }
     
     //LARGE DOT
